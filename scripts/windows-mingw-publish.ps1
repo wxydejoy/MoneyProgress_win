66d1eae1c50ceebab48f416cb6ac151aafae2859
weiekko <1798923421@qq.com>
2023-05-28 00:35:47 +0800

    添加打包脚本

---
+++ scripts/windows-mingw-publish.ps1
@@ -0,0 +1,46 @@
+[CmdletBinding()]
+param (
+    [string] $archiveName, [string] $targetName
+)
+# 外部环境变量包括:
+# archiveName: ${{ matrix.qt_ver }}-${{ matrix.qt_arch }}
+
+
+# archiveName: 5.15.2-win64_mingw81
+
+$scriptDir = $PSScriptRoot
+$currentDir = Get-Location
+Write-Host "currentDir" $currentDir
+Write-Host "scriptDir" $scriptDir
+
+function Main() {
+
+    New-Item -ItemType Directory $archiveName
+    # 拷贝exe
+    Copy-Item bin\release\$targetName $archiveName\
+    
+    # 拷贝额外的文件：主要用于网络访问
+    Copy-Item ExtBin\*.dll $archiveName\
+    Copy-Item ExtBin\*.exe $archiveName\
+    # libcrypto-1_1-x64.dll
+    # cp ExtBin/libssl-1_1-x64.dll libssl-1_1-x64.dll
+    # cp ExtBin/msvcr100.dll msvcr100.dll  win7 64位使用
+    
+    #$Database=Database -f
+    Copy-Item Database $archiveName\Database -recurse
+    
+    # 拷贝依赖
+    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    # 删除不必要的文件
+    $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
+    Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    # 打包zip
+    Compress-Archive -Path $archiveName $archiveName'.zip'
+}
+
+if ($null -eq $archiveName || $null -eq $targetName) {
+    Write-Host "args missing, archiveName is" $archiveName ", targetName is" $targetName
+    return
+}
+Main
+
---
+++ scripts/windows-publish.ps1
@@ -0,0 +1,66 @@
+[CmdletBinding()]
+param (
+    [string] $archiveName, [string] $targetName
+)
+# 外部环境变量包括:
+# archiveName: ${{ matrix.qt_ver }}-${{ matrix.qt_arch }}
+# winSdkDir: ${{ steps.build.outputs.winSdkDir }}
+# winSdkVer: ${{ steps.build.outputs.winSdkVer }}
+# vcToolsInstallDir: ${{ steps.build.outputs.vcToolsInstallDir }}
+# vcToolsRedistDir: ${{ steps.build.outputs.vcToolsRedistDir }}
+# msvcArch: ${{ matrix.msvc_arch }}
+
+
+# winSdkDir: C:\Program Files (x86)\Windows Kits\10\ 
+# winSdkVer: 10.0.19041.0\ 
+# vcToolsInstallDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.28.29333\ 
+# vcToolsRedistDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.28.29325\ 
+# archiveName: 5.9.9-win32_msvc2015
+# msvcArch: x86
+
+$scriptDir = $PSScriptRoot
+$currentDir = Get-Location
+Write-Host "currentDir" $currentDir
+Write-Host "scriptDir" $scriptDir
+
+function Main() {
+
+    New-Item -ItemType Directory $archiveName
+    # 拷贝exe
+    Copy-Item bin\release\$targetName $archiveName\
+    
+    # 拷贝额外的文件：主要用于网络访问
+    Copy-Item ExtBin\*.dll $archiveName\
+    Copy-Item ExtBin\*.exe $archiveName\
+    Copy-Item ExtBin\OCAT.bat $archiveName\OCAT.bat
+    # libcrypto-1_1-x64.dll
+    # cp ExtBin/libssl-1_1-x64.dll libssl-1_1-x64.dll
+    # cp ExtBin/msvcr100.dll msvcr100.dll  win7 64位使用
+    
+    #$Database=Database -f
+    Copy-Item Database $archiveName\Database -recurse
+    
+    #$devDatabase=devDatabase -f
+    #Copy-Item devDatabase $archiveName\devDatabase -recurse
+    
+    # 拷贝依赖
+    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    # 删除不必要的文件
+    $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
+    Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    # 拷贝vcRedist dll
+    $redistDll="{0}{1}\*.CRT\*.dll" -f $env:vcToolsRedistDir.Trim(),$env:msvcArch
+    Copy-Item $redistDll $archiveName\
+    # 拷贝WinSDK dll
+    $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir.Trim(),$env:winSdkVer.Trim(),$env:msvcArch
+    Copy-Item $sdkDll $archiveName\
+    # 打包zip
+    Compress-Archive -Path $archiveName $archiveName'.zip'
+}
+
+if ($null -eq $archiveName || $null -eq $targetName) {
+    Write-Host "args missing, archiveName is" $archiveName ", targetName is" $targetName
+    return
+}
+Main
+
